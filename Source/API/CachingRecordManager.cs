b3a3add680b3222addc7536f278fba958e364f79
leodsan <helloleonardosantiago@gmail.com>
2013-01-27 18:15:18 -0500

    Some changes to the caching record manager:
    1. We populate all of the records from the database into the cache
    2. We expose the values in the cache to RecordManager subclasses

--- Source/API/CachingRecordManager.cs
+++ Source/API/CachingRecordManager.cs
@@ -16,48 +16,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using log4net;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
 
 namespace Aura {
 	/// <summary>
-	/// Works like a standard RecordManager, except that it keeps a cache of all retrieved records.  Well suited for lookups collections
-	/// that have a relatively small number of objects that are frequently retrieved for read-only activity.  
+	/// Works like a standard RecordManager, except that it keeps a cache of all records.  Well suited for lookups collections
+	/// that have a relatively small number of objects that are frequently retrieved for read-only activity.
 	/// </summary>
 	/// <remarks>Calling Save() will in addition to saving the object, also update the item in the cache. Calling Delete() will also remove the item from the cache.</remarks>
 	public class CachingRecordManager<T> : RecordManager<T> where T : class {
         private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		private static Dictionary<object, T> cache = new Dictionary<object, T>();
+		private Dictionary<object, T> cache = new Dictionary<object, T>();
 
+        private bool hasPopulatedCache = false;
+
+        /// <summary>
+        /// Creates a new RecordManager
+        /// </summary>
         public CachingRecordManager(bool isDbManaged)
             : base(isDbManaged)
         {
-
         }
 
+        /// <summary>
+        /// Creates a new CachingRecordManager
+        /// </summary>
         public CachingRecordManager(string collectionName = null, string connectionName = null)
             : base(collectionName, connectionName)
         {
-
         }
 
 		/// <summary>
 		/// Retrieves an object by Id and caches the retrieval by Id for future lookup.
 		/// </summary>
 		public override T GetById(object Id) {
-			if (!cache.ContainsKey(Id)) {
-				lock (cache) {
-					if (!cache.ContainsKey(Id)) {
-						logger.Debug(String.Format("Cache miss on GetById({0}). performing lookup.", Id));
-						var result = base.GetById(Id);
-						if (result == null) {
-							return null;
-						}
-						cache.Add(Id, result);
-					}
-				}
-			}
+
+            if (!hasPopulatedCache)
+            {
+                PopulateCache();
+            }
+
 			return cache[Id];
 		}
+
+        /// <summary>
+        /// Gets all items in database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> GetAll()
+        {
+            if (!hasPopulatedCache)
+            {
+                PopulateCache();
+            }
+
+            return cache.Values.ToList();
+        }
+
+        protected IEnumerable<T> Values
+        {
+            get
+            {
+                if (!hasPopulatedCache)
+                {
+                    PopulateCache();
+                }
+
+                return cache.Values;
+            }
+        }
+
+        private void PopulateCache()
+        {
+            lock (cache)
+            {
+                if (!hasPopulatedCache)
+                {
+                    var items = Find(Query.Null);
+
+                    foreach (var item in items)
+                    {
+                        var id = GetRecordId(item);
+                        cache.Add(id, item);
+                    }
+
+                    hasPopulatedCache = true;
+                }
+            }
+        }
 
 		/// <summary>
 		/// Removes all items from the cache.
@@ -66,18 +115,7 @@ namespace Aura {
 			logger.Debug("ClearCache()");
 			lock (cache) {
 				cache.Clear();
-			}
-		}
-
-		/// <summary>
-		/// Removes a single item from the cache.
-		/// </summary>
-		public void ClearItem(object Id) {
-			logger.Debug(String.Format("ClearItem({0})", Id));
-			lock (cache) {
-				if(cache.ContainsKey(Id)) {
-					cache.Remove(Id);
-				}
+                hasPopulatedCache = false;
 			}
 		}
 
@@ -88,7 +126,10 @@ namespace Aura {
 			logger.Debug(String.Format("DeleteById({0})", id));
 			base.DeleteById(id);
 			if (cache.ContainsKey(id)) {
-				cache.Remove(id);
+                lock (cache)
+                {
+                    cache.Remove(id);
+                }
 			}
 		}
 
