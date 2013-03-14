7152775f9f169336a983d9fcddb3332ad44492ca
leodsan <helloleonardosantiago@gmail.com>
2013-03-14 15:34:28 -0400

    Add support for NuGet Package
    Add NuGet package support

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -5,8 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mongol")]
-[assembly: AssemblyProduct("Mongol")]
+[assembly: AssemblyTitle("Aura")]
+[assembly: AssemblyProduct("Aura")]
+[assembly: AssemblyCompany("Reaktix")]
+[assembly: AssemblyDescription("Wrapper around MongoDB .NET driver to make your life easier")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -18,5 +20,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.0.*")]
-[assembly: AssemblyInformationalVersion("0.15.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
